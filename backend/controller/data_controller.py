930bc635be47ba1e6cdbcf038c071110344dd554
monjojane <e0774715@u.nus.edu>
2024-04-05 13:31:54 +0800

    i edited the backend part abit

--- backend/controller/data_controller.py
+++ backend/controller/data_controller.py
@@ -6,18 +6,40 @@ app = Flask(__name__)
 @app.route('/api/data', methods=['GET'])
 def get_data():
     # To add model logic to get data from database and transform data.
-    data = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9]}
+    #data = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9]}
+    file_path = '../data/bundled_discount.csv'
+    data = pd.read_csv(file_path)
+
     df = pd.DataFrame(data)
     df_json = df.to_json(orient="records")
     return jsonify(df_json)
 
 @app.route('/api/data', methods=['POST'])
 def post_data():
-    data = request.json
     # Here you would process the data and perhaps return a response
-    return jsonify({'status': 'success', 'received_data': data})
+    file_path = '../data/bundled_discount.csv'
+    data = pd.read_csv(file_path)
+    columns_order = data.columns.tolist()
+    new_data = request.json
 
-@app.route('/api/data', methods=['PATCH'])  # Changed from POST to PATCH for semantic correctness
+    # Extract data in the correct order
+    new_row = [new_data[col] for col in columns_order]
+
+    # Convert the list to a comma-separated string
+    new_row_str = ','.join(map(str, new_row))
+
+    with open(file_path, 'a') as file:
+        file.write(new_row_str + '\n')
+
+    return jsonify({'status': 'success', 'received_data': new_row_str})
+
+    print("Appending new row:", new_row_str)
+    with open(file_path, 'a') as file:
+        file.write(new_row_str + '\n')
+    print("New row appended")
+
+
+'''@app.route('/api/data', methods=['PATCH'])  # Changed from POST to PATCH for semantic correctness
 def patch_data():
     data = request.json
     item_id = data.get('item_id')  # Assume that the item_id is part of the incoming JSON data
@@ -27,6 +49,6 @@ def patch_data():
         data_store[item_id] = new_value
         return jsonify({'status': 'success', 'updated_data': {item_id: new_value}})
     else:
-        return jsonify({'status': 'error', 'message': 'Item not found'}), 404
+        return jsonify({'status': 'error', 'message': 'Item not found'}), 404'''
 
 
