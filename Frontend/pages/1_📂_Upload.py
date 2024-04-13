e1716c5c5d85a88f85086ad01ccf72cfab51bf2a
Jheong Rong Yan <e0774299@u.nus.edu>
2024-04-13 16:19:14 +0800

    change update and upload page, add delete method in app.py

--- Frontend/pages/1_📂_Upload.py
+++ Frontend/pages/1_📂_Upload.py
@@ -1,21 +1,45 @@
 import streamlit as st
 import pandas as pd
 from io import StringIO
+import app
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+st.title("Upload your data here!")  # add a title
+with st.expander("Overseas Cable Car"):
+    uploaded_file = st.file_uploader("Choose a file", key=1)
+    if uploaded_file is not None:
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file)
+        st.write(dataframe)
+        if st.button('Update', key=5):
+            json = dataframe.to_json(orient ='records')
+            app.update_data(json, 'overseas')
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+with st.expander("Bundle packages"):
+    uploaded_file = st.file_uploader("Choose a file", key=2)
+    if uploaded_file is not None:
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file)
+        st.write(dataframe)
+        if st.button('Update', key=6):
+            json = dataframe.to_json(orient ='records')
+            app.update_data(json, 'allisbundle')
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
+with st.expander("Citizen (Single Attractions)"): 
+    uploaded_file = st.file_uploader("Choose a file", key=3)
+    if uploaded_file is not None:
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file)
+        st.write(dataframe)
+        if st.button('Update', key=7):
+            json = dataframe.to_json(orient ='records')
+            app.update_data(json, 'citsingle')
 
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+with st.expander("Non-citizen (Single Attractions)"): 
+    uploaded_file = st.file_uploader("Choose a file", key=4)
+    if uploaded_file is not None:
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file)
+        st.write(dataframe)
+        if st.button('Update', key=8):
+            json = dataframe.to_json(orient ='records')
+            app.update_data(json, 'noncitsingle')
